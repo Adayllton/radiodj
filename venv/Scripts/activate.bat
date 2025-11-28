a4b9a90143a986155aebe7385bd38911cf30f9b3
Adayllton <agssdking@gmail.com>
2025-11-28 16:51:05 -0300

    spotipy

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Adayllton\Downloads\dj_ia_streamlit\dj_ia_streamlit\venv
+set VIRTUAL_ENV=C:\Users\Adayllton\Documents\GitHub\radiodj\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
